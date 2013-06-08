6460070ece30cf39c0fac5ca41f8b74c34dc3a67
Josh Kunz <joshkunz@me.com>
2013-06-07 22:09:55 -0600

    Added javascript version

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+/* To Run:
+ * $ node fizzbuzz.js */
+
+for (i = 0; i < 101; i++) {
+    out = i + " ";
+    if (i % 3 == 0) {
+        out += "Fizz";
+    }
+    if (i % 5 == 0) {
+        out += "Buzz";
+    }
+    console.log(out);
+}
