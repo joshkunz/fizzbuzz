1944c22002e37e7d79ea1b4a0b700bfb442765b0
Josh Kunz <joshkunz@me.com>
2013-06-07 22:18:52 -0600

    Added shell version

---
+++ fizzbuzz.sh
@@ -0,0 +1,10 @@
+for (( i=0 ; i <= 100 ; i++ )) do
+    v="$i "
+    if [ $(( i % 3 )) -eq 0 ]; then 
+        v="${v}Fizz"
+    fi
+    if [ $(( i % 5 )) -eq 0 ]; then
+        v="${v}Buzz"
+    fi
+    echo $v
+done
