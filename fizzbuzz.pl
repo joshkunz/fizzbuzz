cdab0df324eac3991007009e06d7153754c82905
Josh Kunz <joshkunz@me.com>
2013-11-05 11:21:55 -0700

    Slight modifications to the prolog example.

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -5,13 +5,14 @@ divs(X,Y) :- 0 is mod(X,Y).
 fizzy(X) :- divs(X,3).
 buzzy(X) :- divs(X,5).
 
-fizzbuzz(X) :- fizzy(X), buzzy(X) -> print('FizzBuzz'); 
-               fizzy(X) -> print('Fizz'); 
-               buzzy(X) -> display('Buzz');
+fizzbuzz(X) :- fizzy(X), buzzy(X) -> print('FizzBuzz');
+               fizzy(X) -> print('Fizz');
+               buzzy(X) -> print('Buzz');
                R is X, print(R). 
 
 fizzbuzzr(From,To) :- From =< To, 
                         fizzbuzz(From), nl,
-                      fizzbuzzr(From+1, To).
+                      fizzbuzzr(From+1, To); 
+                      true.
 
-main :- \+ fizzbuzzr(1,100), halt.
+main :- fizzbuzzr(1,100), halt.
