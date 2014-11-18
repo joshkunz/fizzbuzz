b66c62c8f91d85e66bda7fb27873240dc01c888b
Josh Kunz <joshkunz@me.com>
2014-11-18 03:43:19 -0700

    Fixes Haskell implementation.

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -3,13 +3,15 @@
 
 module Main (main) where
 
--- Return the given string if y is evenly divisible by x
-pdivt :: (Integral a) => String -> a -> a -> String
-pdivt t x y = if (mod y x == 0) then t else ""
+divs :: (Integral a) => a -> a -> Bool
+divs x y = (mod x y) == 0
 
 -- Get the output line as a string for a number
 fbs :: (Integral a, Show a) => a -> String
-fbs x = (show x) ++ " " ++ (pdivt "Fizz" 3 x) ++ (pdivt "Buzz" 5 x)
+fbs x | (divs x 3) && (divs x 5) = "FizzBuzz"
+fbs x | (divs x 3) = "Fizz"
+fbs x | (divs x 5) = "Buzz"
+fbs x = show x
 
 -- Get the IO operation for a particular number
 fb :: (Integral a, Show a) => a -> IO ()
